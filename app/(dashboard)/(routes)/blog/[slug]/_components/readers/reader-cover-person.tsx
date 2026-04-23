4399dcffe1d1329063f076bd70b8b4f9b8c46399
JTellez <jbautistat@unal.edu.co>
2026-04-23 14:13:28 -0500

    Blog

--- app/(dashboard)/(routes)/blog/[slug]/_components/readers/reader-cover-person.tsx
+++ app/(dashboard)/(routes)/blog/[slug]/_components/readers/reader-cover-person.tsx
@@ -34,6 +34,7 @@ export function ReaderCoverPerson({ article, viewCount, articleId, articleSlug, 
                     article={article as any}
                     viewCount={viewCount}
                     articleId={articleId}
+                    articleSlug={articleSlug}
                     prev={prev}
                     next={next}
                 />
